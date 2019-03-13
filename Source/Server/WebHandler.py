9e125ea6614e1bcee2d583d00d9485dd91f30f5b
u296 <42377419+u296@users.noreply.github.com>
2019-03-13 16:38:22 +0000

    lål

---
+++ Source/Server/WebHandler.py
@@ -0,0 +1,12 @@
+from aiohttp import web
+
+async def GetHandler(request):
+    pass
+
+async def PostHandler(request):
+    pass
+
+class WebServer:
+    """
+    Container for web server, use run() method to start
+    """
